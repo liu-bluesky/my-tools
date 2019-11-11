35b0ccc1f7fefea7f7c37d0ca20e14dac1d69338
liu-bluesky <229236940@qq.com>
2019-11-11 18:46:06 +0800

    init

---
+++ 地址智能识别/demo/demo.js
@@ -0,0 +1,10 @@
+var 
+    {
+        smart
+    }
+ = require('./index/index.min.js');
+// var {
+//     smart
+// } = require('./js/address_parse');
+
+console.log(smart('河南郑州金水区三全路柳林村66号18336465466刘蓝天'));
