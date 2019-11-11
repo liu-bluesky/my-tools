3fecd4971830a531b30ad77c7d0c1af6c99beb3f
liu-bluesky <229236940@qq.com>
2019-11-11 19:29:57 +0800

    独立输出时间

--- 倒计时插件/倒计时纯js插件/time-format.js
+++ 倒计时插件/倒计时纯js插件/time-format.js
@@ -2,33 +2,30 @@
 //  * 获取当前时间2 1 24 32 = 5072
 //  */
 // 开始时间
-var start = 1573461745
-var end = 1573461909
+// var start = 1573461745
+// var end = 1573461909
 // var 
-var xiangcha = end-start
-function s(xiangcha){
-	
+// var xiangcha = end-start
+function countDown(xiangcha) {
 	var day = Math.floor(xiangcha / 60 / 60 / 24).toString()
-	
 	// 求出天后面的秒
-	var sAll1 = xiangcha - (day*24*3600)
-	 // var hour =parseInt(xiangcha / 60 / 60 % 24)
-	  var hour =Math.floor(sAll1/3600).toString()
+	var sAll1 = xiangcha - (day * 24 * 3600)
+	// var hour =parseInt(xiangcha / 60 / 60 % 24)
+	var hour = Math.floor(sAll1 / 3600).toString()
 	// 求出小时后面的秒
-	var sAll2 = sAll1 - (hour*60*60)
+	var sAll2 = sAll1 - (hour * 60 * 60)
 	console.log(sAll2);
-	var minute =Math.floor(sAll2/60).toString()
+	var minute = Math.floor(sAll2 / 60).toString()
 	// 求出分钟后面的秒
-	var s = sAll2 - (minute*60).toString()
-	return (day.length<2?'0'+day:day)+"天"+(hour.length<2?'0'+hour:hour)+"小时"+(minute.length<2?'0'+minute:minute)+"分"+(s.length<2?'0'+s:s)+"秒"
+	var s = sAll2 - (minute * 60).toString()
+	var objTime = {
+		day: day.length < 2 ? '0' + day : day,
+		hour: hour.length < 2 ? '0' + hour : hour,
+		minute: minute.length < 2 ? '0' + minute : minute,
+		s: s.length < 2 ? '0' + s : s
+	}
+	return objTime
 }
- var time =	setInterval(function(){
-	 if(xiangcha ==0){
-	 	clearInterval(time)
-	 }
-	s(xiangcha)
-	xiangcha--
-},50)
-
-
-
+module.exports = {
+	countDown
+}
